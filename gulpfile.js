5820478fd75a41413b4e737da037c1012c745300
Zhuang Liang <zhuang@uber.com>
2015-10-09 11:13:18 -0700

    pro-tip: set-up livereload without using sublime livereload, but using gulp-watch and gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');  // runs a local dev server
 var open = require('gulp-open');  // open a url in a web browser
+var watch = require('gulp-watch');
 
 
 var config = {
@@ -24,12 +25,10 @@ gulp.task('connect', function() {
     });
 })
 
-
 gulp.task('open', ['connect'], function() {
     gulp.src('dist/index.html')
         .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
 });
-
 
 gulp.task('html', function() {
     gulp.src(config.paths.html)
@@ -49,5 +48,9 @@ gulp.task('js', function() {
         .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'js', 'css', 'open']);
+gulp.task('watch', function() {
+    gulp.watch('src/*', ['html', 'js', 'css']);
+    watch(['dist/**']).pipe(connect.reload());
+});
+gulp.task('default', ['watch', 'open']);
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ var React = require('react');
 var Main = require('./main');
 
 console.log('blah');
+
 React.render(<Main />, document.getElementById('root'));
