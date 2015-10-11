e9d2ffa7e3c347394374394f5960ff90e7b6a395
Zhuang Liang <zhuang@uber.com>
2015-10-11 09:44:44 -0700

    added transition into this page and away with a simple confirm prompt

--- src/components/author-page.js
+++ src/components/author-page.js
@@ -8,6 +8,21 @@ var AuthorList = require('./author-list');
 // does the rendering with data
 // the separation keeps the logic compact and the rendering all in one place
 var AuthorPage = React.createClass({
+    statics: {  // TIP: statics, not static
+        willTransitionTo: function(transition, params, query, callback) {
+            if (!confirm('are you sure? it\'s really boring')) {
+                transition.abort();
+            } else {
+                callback();  // make the transition
+            }
+        },
+        willTransitionFrom: function(transition, component) {
+            if (!confirm('are you sure? it\'s really exciting')) {
+                transition.abort();
+            }
+        }
+    },
+
     getInitialState: function() {
         return {
             authors: []
