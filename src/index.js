4e0af099a7b5e28dfc28b7b3b2e97a00e40bec20
Zhuang Liang <zhuang@uber.com>
2015-10-11 12:56:28 -0700

    problem: disabled Router.HistoryLocation because it breaks a lot of things

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-Router.run(routes, Router.HistoryLocation, function(Handler) {
+// Router.HistoryLocation breaks alot of things. need to figure out how to use it
+// Router.run(routes, Router.HistoryLocation, function(Handler) {
+Router.run(routes, function(Handler) {
     React.render(<Handler />, document.getElementById('root'));
 });
 
--- src/routes.js
+++ src/routes.js
@@ -11,9 +11,9 @@ var Route = Router.Route;
 var routes = (
     <Route name='app' path='/' handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/home-page')} />
-        <Route name='click-counter' handler={require('./components/click-counter-page')} />
-        <Route name='authors' handler={require('./components/author-page')} />
-        <NotFoundRoute handler={require('./components/not-found-page')} />
+        <Route name='click-counter' path='click' handler={require('./components/click-counter-page')} />
+        <Route name='authors' path='authors' handler={require('./components/author-page')} />
+        <NotFoundRoute path='404' handler={require('./components/not-found-page')} />
         <Redirect from='author-detail' to='authors'/>
         <Redirect from='authors/*' to='authors'/>
     </Route>
