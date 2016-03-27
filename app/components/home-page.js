d670de0a96d1aeb6152cb862e3cd5ff9100c6afb
Zhuang Liang <zhuang@uber.com>
2016-03-27 14:07:03 -0700

    iframe kopf

--- app/components/home-page.js
+++ app/components/home-page.js
@@ -4,9 +4,13 @@ var React = require('react');
 
 var Home = React.createClass({
     render: function() {
+        // need a plugin to drop X-FRAME-OPTIONS:sameorigin
+        var site = 'https://stargate.uberinternal.com/rtsearch_sjc1/elasticsearch/_plugin/kopf/#!/cluster';
         return (
             <div className="text--center giga">
-                <h1 className="wrapper soft push">ELK</h1>
+                <iframe src={site} width="1200" height="1200">
+                  <p>Your browser does not support iframes.</p>
+                </iframe>
             </div>
         );
     }
